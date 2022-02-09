629a08ab060b8750ea1cd5d16656e6eae1b4870d
Shah-Jahan-Mukhtar <shahjahanmukhtar62@gmail.com>
2022-02-09 15:33:24 -0800

    Forms

--- AwsomeProject/screens/Forms.js
+++ AwsomeProject/screens/Forms.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 import {
   Text,
   ScrollView,
@@ -8,22 +7,49 @@ import {
   TextInput,
 } from "react-native";
 
-const [value1, setValue1] = useState("");
-const [value2, setValue2] = useState("");
-const [value3, setValue3] = useState("");
-const [value4, setValue4] = useState("");
-
 const Forms = () => {
+  const [value1, setValue1] = useState("");
+  const [value2, setValue2] = useState("");
+  const [value3, setValue3] = useState("");
+  const [value4, setValue4] = useState("");
   return (
     <SafeAreaView style={{ marginTop: 25 }}>
       <ScrollView style={styles.container}>
-        <Text>Basic Text input:</Text>
-        <Text>Current Value:{value1}</Text>
+        <Text>Basic Text input:🤫</Text>
+        <Text>Current Value :{value1}</Text>
         <TextInput
           style={styles.input}
           value={value1}
           onChangeText={setValue1}
-          placeholdler="Please Input here"
+          placeholder="Enter Input here"
+        />
+        <Text>Number Input :🤫</Text>
+        <Text>Current Value :{value2} </Text>
+        <TextInput
+          style={styles.input}
+          value={value2}
+          onChangeText={setValue2}
+          placeholder="Please Enter Number"
+          keyboardType="numeric"
+        />
+        <Text>Password Input:🤫</Text>
+        <Text>Current Value:{value3}</Text>
+        <TextInput
+          style={styles.input}
+          value={value3}
+          onChangeText={setValue3}
+          secureTextEntry={true}
+          placeholder="Please Enter Password"
+        />
+        <Text>Multiline Input</Text>
+        <Text>Current Value:{value4} </Text>
+        <TextInput
+          style={styles.input}
+          value={value4}
+          onChangeText={setValue4}
+          placeholder="Please Enter The text"
+          multiline={true}
+          numberOfLines={4}
         />
       </ScrollView>
     </SafeAreaView>
@@ -36,9 +62,9 @@ const styles = StyleSheet.create({
     borderColor: "black",
     padding: 5,
     marginBottom: 20,
+    borderRadius: 8,
   },
   container: {
-    flex: 1,
     padding: 5,
   },
 });
